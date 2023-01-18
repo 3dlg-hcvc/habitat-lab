adb5ceca5269eeb6231312d6d0c3f35b435ff0de
Mukul Khanna <mukul18khanna@gmail.com>
2023-01-18 18:05:38 -0500

    Updates helper script for v0.2.0

--- data/scripts/floorplanner/common/extract_object_ids_for_semantic_categories.py
+++ data/scripts/floorplanner/common/extract_object_ids_for_semantic_categories.py
@@ -5,21 +5,22 @@ import pandas as pd
 import yaml
 from tqdm import tqdm
 
+version_id = "v0.2.0"
+
 goal_categories_file_path = (
-    "data/scene_datasets/floorplanner/v1/goal_categories.yaml"
+    f"data/scene_datasets/floorplanner/goals/6_goal_categories.yaml"
 )
-model_stats_path = (
-    "data/scene_datasets/floorplanner/v1/stats/fp_model_stats.tsv"
+model_stats_path = f"data/scene_datasets/floorplanner/{version_id}/configs/semantics/object_metadata.csv"
+semantics_out_path = (
+    f"data/scene_datasets/floorplanner/{version_id}/configs/semantics"
 )
-semantics_out_path = "data/scene_datasets/floorplanner/v1/configs/semantics"
-os.makedirs(semantics_out_path, exist_ok=True)
 
 with open(goal_categories_file_path, "r") as f:
     goal_categories = sorted(yaml.safe_load(f))
 
-data = pd.read_csv(model_stats_path, sep="\t")
+data = pd.read_csv(model_stats_path)
 
 for cat in tqdm(goal_categories):
-    cat_obj_ids = data.loc[data["category"] == cat]["id"].to_list()
+    cat_obj_ids = data.loc[data["main_category"] == cat]["id"].to_list()
     with open(os.path.join(semantics_out_path, f"{cat}.yaml"), "w") as f:
         yaml.dump(cat_obj_ids, f)
