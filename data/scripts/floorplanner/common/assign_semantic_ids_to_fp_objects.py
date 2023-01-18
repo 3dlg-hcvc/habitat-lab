f417c244891b7439cd7b30b29e4ee38381f0899f
Mukul Khanna <mukul18khanna@gmail.com>
2023-01-18 17:31:51 -0500

    Updates assign_semantic_id_script

--- data/scripts/floorplanner/common/assign_semantic_ids_to_fp_objects.py
+++ data/scripts/floorplanner/common/assign_semantic_ids_to_fp_objects.py
@@ -1,68 +1,43 @@
 import json
 import os
 
+import pandas as pd
 import yaml
 from tqdm import tqdm
 
-# path containing individual .txt files containing list of object IDs for each semantic category
-semantics_path = "data/scene_datasets/floorplanner/v1/configs/semantics"
+version_id = "v0.2.0"
+obj_metadata_path = f"data/scene_datasets/floorplanner/{version_id}/configs/semantics/object_metadata.csv"
+objects_path = f"data/scene_datasets/floorplanner/{version_id}/configs/objects"
 goal_categories_path = (
-    "data/scene_datasets/floorplanner/v1/goal_categories.yaml"
+    "data/scene_datasets/floorplanner/goals/33_goal_categories.yaml"
 )
-objects_path = "data/scene_datasets/floorplanner/v1/configs/objects"
+semantic_id_mapping_path = f"data/scene_datasets/floorplanner/{version_id}/configs/object_semantic_id_mapping.json"
 
 
 if __name__ == "__main__":
-    semantic_dict = {}
+    obj_metadata = pd.read_csv(obj_metadata_path)
 
-    ## create semantic dict
+    with open(semantic_id_mapping_path, "r") as f:
+        semantic_id_mapping = json.load(f)
+
     with open(goal_categories_path, "r") as f:
         goal_categories = yaml.safe_load(f)
 
-    MISC_CATEGORY_ID = len(goal_categories)
-
     for cat in goal_categories:
-        file_path = os.path.join(semantics_path, cat + ".yaml")
-        assert os.path.exists(file_path), file_path
+        cat_semantic_id = semantic_id_mapping[cat]
+        obj_ids = obj_metadata[obj_metadata["main_category"] == cat][
+            "id"
+        ].tolist()
+        assert len(obj_ids) == len(set(obj_ids))  # assert no duplicate ids
 
-        with open(file_path, "r") as f:
-            cat_ids = yaml.safe_load(f)
-            semantic_dict[cat] = cat_ids
+        for obj_id in tqdm(obj_ids, desc=f"{cat} ({cat_semantic_id})"):
+            obj_json_path = os.path.join(
+                objects_path, obj_id + ".object_config.json"
+            )
+            with open(obj_json_path, "r") as f:
+                data = json.load(f)
+                assert "semantic_id" not in data.keys()
+                data["semantic_id"] = cat_semantic_id
 
-    print("Number of objects in each category:")
-    for key in semantic_dict.keys():
-        print(key, len(semantic_dict[key]))
-
-    print("-" * 40)
-
-    print("Category semantic ID mapping:")
-    for key in semantic_dict.keys():
-        print(key, goal_categories.index(key))
-
-    ## assign semantic IDs
-    print("Assigning semantic IDs:")
-    objects = [x for x in os.listdir(objects_path) if x.endswith(".json")]
-    misc_ctr = 0
-
-    for obj in tqdm(objects):
-        obj_path = os.path.join(objects_path, obj)
-        obj = obj.split(".")[0]
-        obj_cat = None
-
-        for key in semantic_dict.keys():
-            if obj in semantic_dict[key]:
-                obj_cat = goal_categories.index(key)
-                break
-
-        if obj_cat is None:
-            obj_cat = MISC_CATEGORY_ID
-            misc_ctr += 1
-
-        with open(obj_path, "r") as f:
-            obj_json = json.load(f)
-            obj_json["semantic_id"] = obj_cat
-
-        with open(obj_path, "w") as f:
-            json.dump(obj_json, f)
-
-    print("Number of non-miscellaneous objects:", len(objects) - misc_ctr)
+            with open(obj_json_path, "w") as f:
+                json.dump(data, f)
