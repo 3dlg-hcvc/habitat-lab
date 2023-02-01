f9220f23612e1b5ac04f9fb53890e9746aa305f3
Yongsen Mao <sammaoys@gmail.com>
2023-02-01 02:19:28 -0800

    add procthor settings

--- data/scripts/floorplanner/objectnav/create_fp_objectnav_dataset.py
+++ data/scripts/floorplanner/objectnav/create_fp_objectnav_dataset.py
@@ -73,6 +73,7 @@ FAILURE_VIZ_FOLDER = os.path.join(
     OUTPUT_DATASET_FOLDER, "viz", "failure_cases"
 )
 NUM_GPUS = len(GPUtil.getAvailable(limit=256))
+NUM_GPUS = 2
 TASKS_PER_GPU = 20
 deviceIds = GPUtil.getAvailable(order="memory")
 
