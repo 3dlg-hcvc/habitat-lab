68a1d959b74d342d09caed654fd7c6fec3ad78ee
Yongsen Mao <sammaoys@gmail.com>
2023-02-01 09:02:31 -0800

    fix gpu device ids assignment

--- data/scripts/floorplanner/objectnav/create_fp_objectnav_dataset.py
+++ data/scripts/floorplanner/objectnav/create_fp_objectnav_dataset.py
@@ -112,9 +112,9 @@ def get_objnav_config(i, scene):
     objnav_config.SIMULATOR.RGB_SENSOR.HEIGHT //= 2
     objnav_config.TASK.MEASUREMENTS = []
 
-    deviceIds = GPUtil.getAvailable(order="memory", limit=1.0, maxLoad=1.0, maxMemory=1.0)
+    deviceIds = GPUtil.getAvailable(order="memory", limit=1, maxLoad=1.0, maxMemory=1.0)
     if i < NUM_GPUS * TASKS_PER_GPU or len(deviceIds) == 0:
-        deviceId = deviceIds[i % len(deviceIds)]
+        deviceId = deviceIds[0]
     else:
         deviceId = deviceIds[0]
     objnav_config.SIMULATOR.HABITAT_SIM_V0.GPU_DEVICE_ID = deviceId
